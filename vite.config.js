933834e4b6e7c08d04efb1c4c5ab34906a59cf3d
张雄 <1549566737@qq.com>
2023-04-11 14:58:41 +0800

    upd: project init

---
+++ vite.config.js
@@ -0,0 +1,54 @@
+import { defineConfig } from "vite";
+import path from "path";
+import vue from "@vitejs/plugin-vue";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+
+const resolve = (dir) => path.resolve(__dirname, dir);
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "/",
+  publicDir: resolve("static"),
+  resolve: {
+    alias: {
+      "@": resolve("src"), //作为 entries 的选项
+    },
+    extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json", ".vue"],
+  },
+  server: {
+    open: true,
+    port: 5566,
+  },
+  build: {
+    //打包环境移除console.log，debugger
+    minify: "terser",
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+    //打包文件按照类型分文件夹显示
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+      },
+      output: {
+        chunkFileNames: "js/[name]-[hash].js",
+        entryFileNames: "js/[name]-[hash].js",
+        assetFileNames: "[ext]/[name]-[hash].[ext]",
+      },
+    },
+  },
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
+});
